d19e0a9a81df6cd75382cccd3072b27bf5ef8fa1
stevechan <chanshunli@gmail>
2020-12-11 15:53:17 +0800

    增加rn文件

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	render() {
+		return (
+			<View>
+				<Text style={styles.text}>Hello, world!</Text>
+			</View>
+		)
+	}
 }
 
-export default App;
+const styles = StyleSheet.create({
+	text: {
+		fontWeight: "bold",
+		fontSize: 30
+	}
+})
+
+export default App
